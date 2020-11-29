714718f99a5c9ae7a369554d82f9b8f4e99df6c9
Henry Peng <peng254@purdue.edu>
2020-11-29 14:31:35 -0500

    Updated Client to have sign in, sign up, and profile change. Assumed: on homeframe: sign in button and register button, username and password textfield on mainframe: profile page button and back button on signupframe: sign up button and back button on profileframe: back button and update profile button, and text fields for profile info
    Signed-off-by: Henry Peng <peng254@purdue.edu>

--- src/Client.java
+++ src/Client.java
@@ -41,23 +41,78 @@ public class Client extends JComponent implements Runnable, ActionListener {
 
     gui.HomeFrame homeFrame = new gui.HomeFrame();
     gui.MainFrame mainFrame = new gui.MainFrame();
+    gui.SignUpFrame signUpFrame = new gui.SignUpFrame();
+    gui.AddFriendFrame addFriendFrame = new gui.AddFriendFrame();
+    gui.FriendsListFrame friendsListFrame = new gui.FriendsListFrame();
+    gui.ProfileFrame profileFrame = new gui.ProfileFrame();
 
     @Override
     public void actionPerformed(ActionEvent e) {
         Object buttonPressed = e.getSource();
 
         if (buttonPressed == homeFrame.enterButton) {
-            mainFrame.setVisible(true);
+            sendMessage(String.format("1\n%s\n%s", homeFrame.username.getText(), homeFrame.password.getText()));
+            String signInResponse = receiveMessage();
+            if (signInResponse.equals("incorrect username or password")) {
+                JOptionPane.showMessageDialog(null, "Incorrect Username or Password",
+                        "ERROR", JOptionPane.ERROR_MESSAGE);
+            } else if (signInResponse.equals("success")) {
+                mainFrame.setVisible(true);
+                homeFrame.dispose();
+            }
+        }
+
+        if (buttonPressed == homeFrame.signUpButton) {
+            signUpFrame.setVisible(true);
             homeFrame.dispose();
         }
 
-        if (buttonPressed == homeFrame.exitButton) {
+        if (buttonPressed == signUpFrame.signUpButton) {
+            sendMessage(String.format("2\n%s\n%s\n%s", signUpFrame.usernameField.getText(),
+                    signUpFrame.passwordField.getText(),
+                    signUpFrame.lastNameField.getText() + ", " + signUpFrame.firstNameField.getText()));
+            String signUpResponse = receiveMessage();
+            if (signUpResponse.equals("Username already taken!")) {
+                JOptionPane.showMessageDialog(null, "Username Taken",
+                        "ERROR", JOptionPane.ERROR_MESSAGE);
+            } else if (signUpResponse.equals("Success")) {
+                mainFrame.setVisible(true);
+                signUpFrame.dispose();
+            }
+        }
 
+        if (buttonPressed == signUpFrame.backButton) {
+            homeFrame.setVisible(true);
+            signUpFrame.dispose();
         }
 
         if (buttonPressed == mainFrame.BACK_BUTTON) {
             homeFrame.setVisible(true);
             mainFrame.dispose();
+        }
+
+        if (buttonPressed == mainFrame.profileButton) {
+            profileFrame.setVisible(true);
+            mainFrame.dispose();
+        }
+
+        if (buttonPressed == profileFrame.updateProfileButton) {
+            sendMessage(String.format("3\n%s\n%s\n%s\n%s", profileFrame.emailField.getText(),
+                    profileFrame.ageField.getText(),
+                    profileFrame.lastNameField.getText(), profileFrame.firstNameField.getText()));
+            String signUpResponse = receiveMessage();
+            if (signUpResponse.equals("Error")) {
+                JOptionPane.showMessageDialog(null, "Error",
+                        "ERROR", JOptionPane.ERROR_MESSAGE);
+            } else if (signUpResponse.equals("Success")) {
+                mainFrame.setVisible(true);
+                signUpFrame.dispose();
+            }
+        }
+
+        if (buttonPressed == profileFrame.backButton) {
+            mainFrame.setVisible(true);
+            profileFrame.dispose();
         }
 
     }
@@ -67,7 +122,16 @@ public class Client extends JComponent implements Runnable, ActionListener {
         homeFrame.setVisible(true);
 
         homeFrame.enterButton.addActionListener(this);
+        homeFrame.signUpButton.addActionListener(this);
+
+        signUpFrame.signUpButton.addActionListener(this);
+        signUpFrame.backButton.addActionListener(this);
+
+        mainFrame.profileButton.addActionListener(this);
         mainFrame.BACK_BUTTON.addActionListener(this);
+
+        profileFrame.backButton.addActionListener(this);
+        profileFrame.updateProfileButton.addActionListener(this);
     }
 
 }

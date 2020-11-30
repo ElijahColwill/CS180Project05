f39bbcb535b2fb0819ea7ea4e3f390001e7a4139
Henry Peng <peng254@purdue.edu>
2020-11-29 23:11:00 -0500

    Changed Client to work with multithreading, added Server and Client Handlers used for testing that are compatible with multithreading.
    Signed-off-by: Henry Peng <peng254@purdue.edu>

--- src/Client.java
+++ src/Client.java
@@ -14,7 +14,13 @@ public class Client extends JComponent implements Runnable, ActionListener {
     PrintWriter writer;
     BufferedReader reader;
 
-    // Constructor for Client, needs port number
+    gui.HomeFrame homeFrame = new gui.HomeFrame();
+    gui.MainFrame mainFrame = new gui.MainFrame();
+    gui.SignUpFrame signUpFrame = new gui.SignUpFrame();
+    gui.AddFriendFrame addFriendFrame = new gui.AddFriendFrame();
+    gui.FriendsListFrame friendsListFrame = new gui.FriendsListFrame();
+    gui.ProfileFrame profileFrame = new gui.ProfileFrame();
+
     public Client(int portNum) throws IOException {
         this.portNum = portNum;
 
@@ -22,6 +28,14 @@ public class Client extends JComponent implements Runnable, ActionListener {
 
         this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.writer = new PrintWriter(socket.getOutputStream());
+    }
+
+    public static void main(String[] args) {
+        try {
+            SwingUtilities.invokeLater(new Client(2400));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void sendMessage(String message) {
@@ -39,19 +53,12 @@ public class Client extends JComponent implements Runnable, ActionListener {
         return "";
     }
 
-    gui.HomeFrame homeFrame = new gui.HomeFrame();
-    gui.MainFrame mainFrame = new gui.MainFrame();
-    gui.SignUpFrame signUpFrame = new gui.SignUpFrame();
-    gui.AddFriendFrame addFriendFrame = new gui.AddFriendFrame();
-    gui.FriendsListFrame friendsListFrame = new gui.FriendsListFrame();
-    gui.ProfileFrame profileFrame = new gui.ProfileFrame();
-
     @Override
     public void actionPerformed(ActionEvent e) {
         Object buttonPressed = e.getSource();
 
         if (buttonPressed == homeFrame.enterButton) {
-            sendMessage(String.format("1\n%s\n%s", homeFrame.username.getText(), homeFrame.password.getText()));
+            sendMessage(String.format("sign in\n%s\n%s", homeFrame.username.getText(), homeFrame.password.getText()));
             String signInResponse = receiveMessage();
             if (signInResponse.equals("incorrect username or password")) {
                 JOptionPane.showMessageDialog(null, "Incorrect Username or Password",
@@ -68,7 +75,7 @@ public class Client extends JComponent implements Runnable, ActionListener {
         }
 
         if (buttonPressed == signUpFrame.signUpButton) {
-            sendMessage(String.format("2\n%s\n%s\n%s", signUpFrame.usernameField.getText(),
+            sendMessage(String.format("sign up\n%s\n%s\n%s", signUpFrame.usernameField.getText(),
                     signUpFrame.passwordField.getText(),
                     signUpFrame.lastNameField.getText() + ", " + signUpFrame.firstNameField.getText()));
             String signUpResponse = receiveMessage();
@@ -97,8 +104,8 @@ public class Client extends JComponent implements Runnable, ActionListener {
         }
 
         if (buttonPressed == profileFrame.updateProfileButton) {
-            sendMessage(String.format("3\n%s\n%s\n%s\n%s", profileFrame.emailField.getText(),
-                    profileFrame.ageField.getText(),
+            sendMessage(String.format("change profile\n%s\n%s\n%s\n%s", profileFrame.emailField.getText(),
+                    profileFrame.aboutMeField.getText(),
                     profileFrame.lastNameField.getText(), profileFrame.firstNameField.getText()));
             String signUpResponse = receiveMessage();
             if (signUpResponse.equals("Error")) {
---
+++ src/TestClientHandler.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TestClientHandler extends Thread {
+
+    final Socket s;
+    final BufferedReader reader;
+    final PrintWriter writer;
+
+    public TestClientHandler(Socket s, BufferedReader r, PrintWriter w) {
+        this.s = s;
+        this.reader = r;
+        this.writer = w;
+    }
+
+    public void run() {
+        System.out.println("Connected");
+
+        while (true) {
+            try {
+                String message = reader.readLine();
+                if (message != null) {
+                    System.out.println(message);
+                    writer.write("success");
+                    writer.println();
+                    writer.flush();
+                    System.out.println("sent response");
+                }
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+}

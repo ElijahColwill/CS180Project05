2c7316b85e3605d3b289f3954a81ce8dfc91ef04
Henry Peng <peng254@purdue.edu>
2020-12-06 22:18:12 -0500

    Fixed bug
    Signed-off-by: Henry Peng <peng254@purdue.edu>

--- src/client/Client.java
+++ src/client/Client.java
@@ -301,7 +301,7 @@ public class Client extends JComponent implements Runnable, ActionListener {
         }
         if (buttonPressed == sendFriendRequestFrame.sendRequestButton) {
             sendMessage(String.format("Send request\n%s\n%s", currentUsername,
-                    String.valueOf(sendFriendRequestFrame.userComboBox.getSelectedIndex())));
+                    String.valueOf(sendFriendRequestFrame.userComboBox.getSelectedItem())));
             //System.out.println(String.valueOf(sendFriendRequestFrame.userComboBox.getSelectedIndex()));
         }
 
